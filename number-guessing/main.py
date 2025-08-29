49d5875f7da9834c32215bf5bb247b73803cd0f9
Andre Gil <thomasgil913@gmail.com>
2025-08-29 09:44:21 -0500

    refactor game and add constants file

--- number-guessing/main.py
+++ number-guessing/main.py
@@ -1,29 +1,40 @@
 import random
 
-MAX_ATTEMPTS = 10
+from constants import MAX_ATTEMPTS
+
+
+def get_answer() -> int:
+    input_number = int(input("Pick a random number from 0 to 100: "))
+
+    if input_number < 0 or input_number > 100:
+        raise Exception("Number must be between 0 and 100")
+
+    return input_number
 
 
 def main():
     attempts = 0
-    random_number = random.randint(0, 100)
 
-    while attempts < MAX_ATTEMPTS:
+    while True:
         attempts += 1
-        user_number = int(input("Pick a random number from 0 to 100: "))
+        hidden_number = random.randint(0, 100)
 
-        if user_number < 0 or user_number > 100:
-            print("Number should be between 0 and 100!")
-
-        if user_number == random_number:
-            print("You guessed it! You won.")
+        if attempts > MAX_ATTEMPTS:
+            print("Max attempts reached! You lost.")
             break
-        elif user_number > random_number:
-            print("Pick a smaller number")
-        elif user_number < random_number:
-            print("Pick a bigger number")
 
-    if attempts > MAX_ATTEMPTS:
-        print("Max attempts reached! You lost.")
+        try:
+            answer = get_answer()
+
+            if answer == hidden_number:
+                print("You won!")
+                break
+            elif answer > hidden_number:
+                print("Pick a smaller number")
+            else:
+                print("Pick a bigger number")
+        except Exception as e:
+            print(f"Error: {e}")
 
 
 if __name__ == "__main__":
