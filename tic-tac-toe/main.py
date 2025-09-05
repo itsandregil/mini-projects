5ec7c3224feed0f5360cdf70377cd4d3851f18a4
Andre Gil <thomasgil913@gmail.com>
2025-09-05 11:13:08 -0500

    Refactor all logic into simple functions

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -1,4 +1,11 @@
-def print_board(board: list[list[str]]):
+import random
+
+Board = list[list[str]]
+Move = tuple[int, int]
+Player = str
+
+
+def print_board(board: Board):
     print("\n".join(map(lambda row: " | ".join(row), board)))
 
 
@@ -8,10 +15,16 @@ def map_position(position: int) -> tuple[int, int]:
     return row, col
 
 
-def main():
-    board = [[" " for _ in range(3)] for _ in range(3)]
-    players = ["X", "O"]
-    current_player = players[0]
+def get_position(position: int, board: Board):
+    row, col = map_position(position)
+    return board[row][col]
+
+
+def switch_turn(current_player: Player) -> str:
+    return "X" if current_player == "O" else "O"
+
+
+def check_winner(board: Board, player: Player) -> bool:
     winning_cases = [
         (1, 2, 3),
         (4, 5, 6),
@@ -22,51 +35,60 @@ def main():
         (1, 5, 9),
         (3, 5, 7),
     ]
-    has_winner = False
-    is_draw = False
+
+    return any(
+        all(
+            value == player for value in map(lambda pos: get_position(pos, board), case)
+        )
+        for case in winning_cases
+    )
+
+
+def check_draw(num_moves: int) -> bool:
+    return num_moves == 9
+
+
+def validate_move(position: int, board: Board) -> None:
+    if position < 1 or position > 9:
+        raise Exception("Invalid move. Positions should be between 1 - 9")
+    if get_position(position, board) != " ":
+        raise Exception("Invalid move. Position is already taken.")
+    return
+
+
+def make_move(board: Board, player: Player) -> Board:
+    while True:
+        try:
+            player_input = int(input(f"Player {player}, choose a position: "))
+            validate_move(player_input, board)
+
+            # If nothing is raised we update the board
+            row, col = map_position(player_input)
+            board[row][col] = player
+            return board
+        except Exception as e:
+            print(f"Error: {e}")
+
+
+def main():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    current_player = random.choice(["X", "O"])
+    moves_count = 0
 
     while True:
-        # Print the board
         print_board(board)
+        board = make_move(board, current_player)
+        moves_count += 1
 
-        # Get player input
-        user_input = int(input(f"Player {current_player}, choose a position: "))
-        row, col = map_position(user_input)
-
-        # Update board
-        if board[row][col] == " ":
-            board[row][col] = current_player
-
-        # Check for winner or draw
-        for case in winning_cases:
-            count_symbol = 0
-            for index in case:
-                row, col = map_position(index)
-                if board[row][col] == current_player:
-                    count_symbol += 1
-
-            if count_symbol == 3:
-                has_winner = True
-
-        num_not_empty_cells = 0
-        for row in board:
-            for value in row:
-                if value != " ":
-                    num_not_empty_cells += 1
-
-        if num_not_empty_cells == 9:
-            is_draw = True
-
-        if has_winner:
+        if check_winner(board, current_player):
             print(f"Player {current_player} wins!")
             break
 
-        if is_draw:
+        if check_draw(moves_count):
             print("It is a draw!")
             break
 
-        # Change turn
-        current_player = "O" if current_player == "X" else "X"
+        current_player = switch_turn(current_player)
 
 
 if __name__ == "__main__":
